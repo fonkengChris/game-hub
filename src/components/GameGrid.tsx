e0b0bfcb2d1ad1e820678c4cab88c6c923e76414
Christian E. Fonkeng <46832200+fonkengChris@users.noreply.github.com>
2023-04-15 13:29:13 +0100

    Clean up the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,17 +11,13 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const {
-    data: games,
-    error,
-    isLoading,
-  } = useGames(gameQuery);
+  const { data: games, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        spacing={3}
+        spacing={6}
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
       >
